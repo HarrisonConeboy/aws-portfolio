aebd7cdfcb286cb9dcc507d13f19edcce957971e
Harrison Coneboy <hconeboy@gmail.com>
2020-08-14 22:15:40 +0100

    Basic main page w/ some graphics

---
+++ src/Intro.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Intro.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import KBMan from './images/kbman.png';
+
+
+function Intro() {
+    return (
+        <Container>
+            <Row className="myIntro">
+                <Col className="introPara">
+                    <p>
+                        <span style={{fontSize:'36px'}}>Greetings!</span>
+                        <br/>
+                        <span style={{fontSize:'28px', paddingLeft:'50px'}}>I'm Harrison Coneboy</span>
+                    </p>
+                </Col>
+                <Col style={{textAlign:'center'}}>
+                    <img src={KBMan} alt="Self Portrait" className="kbman"/>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Intro
---
+++ src/NavBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './navbar.css';
+import HFace from './images/hFace.svg';
+import Navbar from 'react-bootstrap/NavBar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+function MyNavBar() {
+    return (
+        <Navbar expand="lg">
+            <Navbar.Brand href="#home">
+                <img src={HFace} alt="Logo" className="logo"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-menu">
+                <Nav className="menu">
+                <Nav.Link href="#home">About Me</Nav.Link>
+                <Nav.Link href="#link">Hosted Work</Nav.Link>
+                <NavDropdown title="Project Breakdowns" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.4">All Projects</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.2">Personal</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">University</NavDropdown.Item>
+                    
+                </NavDropdown>
+            </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default MyNavBar
+
